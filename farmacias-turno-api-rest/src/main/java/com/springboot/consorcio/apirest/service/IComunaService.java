2f457583b1bd613f70e081feab1245c619a7cbe8
msolisp <max.solis@gmail.com>
2020-02-03 07:45:39 -0300

    commit inicial api rest

---
+++ farmacias-turno-api-rest/src/main/java/com/springboot/consorcio/apirest/service/IComunaService.java
@@ -0,0 +1,16 @@
+package com.springboot.consorcio.apirest.service;
+
+
+/**
+ * @author maximiliano
+ *
+ */
+public interface IComunaService {
+	
+	/**
+	 * @author maximiliano
+	 *
+	 */
+	public String getComunas(String reg_id);
+
+}
